fc6b497e6a63f6630f904b7bb526119c6cea0f7d
Santosh Kumar Yadav <santosh93ee@gmail.com>
2023-04-10 12:15:39 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { FormsModule } from '@angular/forms';
+import { AboutComponent } from './MyComponent/about/about.component';
+import { HomeComponent } from './MyComponent/home/home.component';
+import { NavbarComponent } from './MyComponent/navbar/navbar.component';
+import { FooterComponent } from './MyComponent/footer/footer.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+  
+    AboutComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
